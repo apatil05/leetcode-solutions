28271d5c74cac45a120c8adabb3b71d65f91068a
Aryan Patil <apatil.connect@gmail.com>
2026-08-30 16:22:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.6 MB (60.90%)

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def binSearch(nums, target, leftBias):
+            res = -1
+            l, r = 0, len(nums) - 1
+            while l <= r:
+                m = l + (r-l)//2
+                if nums[m] == target:
+                    res = m
+                    if leftBias:
+                        r = m-1
+                    else:
+                        l = m+1
+                elif target > nums[m]:
+                    l = m+1
+                else:
+                    r = m-1
+            return res
+            
+        
+        left = binSearch(nums,target,True)
+        right = binSearch(nums,target,False)
+        return [left,right]
